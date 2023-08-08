b4f192fb7bcfae8aac19ba371a4d57f790a22502
Krishnan S <skrishnan2001@gmail.com>
2023-08-08 03:59:10 +0000

    feat: Initial commit

---
+++ src/main/java/com/RoomFour/FinancialPortfolio/FinancialPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.RoomFour.FinancialPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RoomFour/FinancialPortfolio/FinancialPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RoomFour.FinancialPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
